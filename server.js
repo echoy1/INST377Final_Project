9c8ba4e4d44e95e8b8afeb3925bd22e952d1cfa3
wtam26 <wtam26@terpmail.umd.edu>
2020-05-07 02:42:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(express.static("/public"));
 app.use(express.static(__dirname + '/public/css'));
 
 app.get('/', function (req, res) {
-  res.sendFile('public/index.html', { root: '.' })
+  res.sendFile('public/search.html', { root: '.' })
 });
-app.get('/index.html', function (req, res) {
-  res.sendFile('public/index.html', { root: '.' })
+app.get('/search.html', function (req, res) {
+  res.sendFile('public/search.html', { root: '.' })
 });
 app.get('/about.html', function (req, res) {
   res.sendFile('public/about.html', { root: '.' })
@@ -23,12 +23,8 @@ app.get('/contact.html', function (req, res) {
   res.sendFile('public/contact.html', { root: '.' })
 });
 
-<<<<<<< HEAD
-function processDataForFrontEnd(req, res) {
-=======
 
 async function processDataForFrontEnd(req, res) {
->>>>>>> master
   const baseURL = "https://data.princegeorgescountymd.gov/resource/7k64-tdwr.json"; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
@@ -44,33 +40,6 @@ async function processDataForFrontEnd(req, res) {
 //
 app
   .route("/api")
-<<<<<<< HEAD
-  .get((req, res) => {
-    // processDataForFrontEnd(req, res)
-    (async () => {
-      const db = await open(dbSettings);
-      const result = await db.all("SELECT * FROM user");
-      console.log("Expected result", result);
-      res.json(result);
-    })();
-  })
-  .post((req, res) => {
-    console.log("/api post request", req.body);
-    if (!req.body.name) {
-      console.log(req.body);
-      res.status("418").send("something went wrong, additionally i am a teapot");
-    } else {
-      writeUser(req.body.name, dbSettings)
-      .then((result) => {
-        console.log(result);
-        res.send("your request was successful"); // simple mode
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-  });
-=======
 .get((req, res) => {
   (async () => {
     const result = await processDataForFrontEnd(req, res)
@@ -78,7 +47,6 @@ app
   })()
 }
 )
->>>>>>> master
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
